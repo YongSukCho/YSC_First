ea9c3b4afef1ddc4d914c0f3b722b585c252c480
YongSukCho <yongsukcho@gmail.com>
2024-05-18 13:22:52 +0900

    Python excercise

--- Test.py
+++ Test.py
@@ -1,2 +1,8 @@
-print("Hello world!!")
-print("Hello python")
+Temperature = int(input("What is the temperature\n"))
+
+if Temperature < 15:
+    print("It's too cold, stay inside")
+elif Temperature < 30:
+    print("It's a nice day, let's go outside")
+else:
+    print("It's too hot, stay inside")
